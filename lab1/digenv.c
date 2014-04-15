b00721a399c1165ae2e2ceb591304a8c42bb5230
Jonas Sköld <jonas.skold.89@gmail.com>
2014-04-15 14:34:02 +0200

    less kind of working

--- lab1/digenv.c
+++ lab1/digenv.c
@@ -3,10 +3,15 @@
 #include <stdio.h> /* definierar bland annat stderr */
 #include <stdlib.h> /* definierar bland annat rand() och RAND_MAX */
 #include <unistd.h> /* definierar bland annat pipe() */
+#include <sys/wait.h> /* definierar bland annat WIFEXITED */
+
+#define READ 0
+#define WRITE 1
+
 int main(int argc, char **argv, char **envp)
 {
 	int pipe_filedesc[2]; //Pipe for communication
-	int child_pid;
+	pid_t child_pid;
 	int return_value;
 
 	return_value = pipe( pipe_filedesc );
@@ -16,36 +21,54 @@ int main(int argc, char **argv, char **envp)
 	}
 
 	child_pid = fork();
-	if (child_pid == -1) {
-		printf("Fork failed\n");
-		exit(-1);
-	}
 
-	if (child_pid == 0) { //Child
-		return_value = close(pipe_filedesc[1]);//Close write pipe
+	if (child_pid == 0) { //Pager child
+		return_value = dup2( pipe_filedesc[ READ ], 0 ); /* STDIN_FILENO == 0 */
+		if (return_value == -1) {
+			printf("Failed duplicating pipe\n");
+			exit(-1);
+		}
+		return_value = close(pipe_filedesc[WRITE]);//Close write pipe
 		if (return_value == -1) {
 			printf("Failed closing pipe\n");
 			exit(-1);
 		}
-		int value;
-		int bytes = read( pipe_filedesc[0], &value, sizeof(value) );
-		if (bytes == -1) {
-			printf("Failed reading\n");
+		return_value = close(pipe_filedesc[READ]);//Close read pipe
+		if (return_value == -1) {
+			printf("Failed closing pipe\n");
 			exit(-1);
 		}
-		printf("%d\n", value);
+
+		char* const argv[2] = {"less", 0};
+
+		execvp("less", argv);
+
+		perror("Exec failed");
+		exit(-1);
+
 	} else { //Parent
-		return_value = close(pipe_filedesc[0]);//Close write pipe
+		if (child_pid == -1) {
+			printf("Fork failed\n");
+			exit(-1);
+		}
+
+		return_value = close(pipe_filedesc[READ]);//Close read pipe
 		if (return_value == -1) {
 			printf("Failed closing pipe\n");
 			exit(-1);
 		}
-		int value = 1;
-		int bytes = write( pipe_filedesc[1], &value, sizeof(value) );
+		char* value = "hello";
+		int bytes = write( pipe_filedesc[WRITE], value, sizeof("hello") - 1 );
 		if (bytes == -1) {
 			printf("Failed writing\n");
 			exit(-1);
 		}
+		return_value = close(pipe_filedesc[WRITE]);//Close read pipe
+		if (return_value == -1) {
+			printf("Failed closing pipe\n");
+			exit(-1);
+		}
+		wait(0);
 	}
 
 	return 0;
