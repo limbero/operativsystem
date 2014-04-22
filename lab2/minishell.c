2425ff94d6769894f142632dd9f5a2720c3340f4
Jonas Sköld <jonas.skold.89@gmail.com>
2014-04-22 19:13:45 +0200

    Can parse input and exits if command is exit

--- lab2/minishell.c
+++ lab2/minishell.c
@@ -3,30 +3,36 @@
 #include <stdio.h> /* definierar bland annat stderr */
 #include <stdlib.h> /* definierar bland annat rand() och RAND_MAX */
 #include <string.h> /* for strcmp */
+#include <ctype.h> /* isspace */
 
+/* Define booleans */
 typedef int bool;
 #define true 1
 #define false 0
 
-char* read_command();
-void parse_command( char** args, bool* background, char* command_prompt);
-bool built_in_command( char* command );
+void read_command(); /* Read input from stdin */
+void parse_command(); /* Parse input string into array */
+
+char command_prompt[100]; /* The command string that the user inputs */
+bool background = false; /* Whether the current command should be run in background or not */
+char* args[10]; /* The command arguments as an array */
+int argc = 0; /* The number of arguments to the command */
 
 int main(int argc, char **argv, char **envp)
 {
 	while ( true ) {
-		char* command_prompt = read_command();
-		bool background;
-		char* args[1000];
-		parse_command( args, &background, command_prompt );
-		if ( built_in_command(args[0]) ) {
-			printf( "command: %s\n", args[0] );
-			//perform cd or exit
-			//if command is exit, exit
-			if ( strcmp(args[0], "exit") == 0 )
-				exit(0);
+		read_command( );
+		parse_command( );
+
+		if ( strcmp(args[0], "exit") == 0 ) { /* if command is exit, exit */
+			exit(0);
+
+		} else if ( strcmp(args[0], "cd") == 0 ) {
+			/* TODO perform cd */
+			
 		} else {
-			//execute system command in child process
+			/* execute system command in child process */
+
 
 		}
 		//print info about finished command
@@ -34,17 +40,25 @@ int main(int argc, char **argv, char **envp)
 	}
 }
 
-char* read_command() {
-	return "command args &";
+void read_command() {
+	char *buf = command_prompt;
+	size_t size = sizeof( command_prompt );
+	getline( &buf, &size, stdin );
 }
 
-void parse_command( char** args, bool* background, char* command_prompt ) {
-	args[0] = "exit";
-	*background = false;
-}
-
-bool built_in_command( char* command ) {
-	if ( strcmp(command, "cd") == 0 || strcmp(command, "exit") == 0 )
-		return true;
-	return false;
+void parse_command() {
+	char * delimiter = " \n"; //Split command string on space and newline
+	char *token = strtok( command_prompt, delimiter );
+	int i = 0;
+    while( token != NULL )
+    {
+    	args[i++] = token;
+        token = strtok( NULL, delimiter );
+    }
+    argc = i;
+	background = false;
+    if ( strcmp( args[i-1], "&" ) == 0 ) {
+    	argc = i - 1;
+    	background = true;
+    }
 }
