ee0fa91c9fb2b098434074ad9e7254d0181965c4
Jonas Sköld <jonas.skold.89@gmail.com>
2014-04-22 19:36:40 +0200

    Execute shell commands and wait for the to finish

--- lab2/minishell.c
+++ lab2/minishell.c
@@ -4,6 +4,8 @@
 #include <stdlib.h> /* definierar bland annat rand() och RAND_MAX */
 #include <string.h> /* for strcmp */
 #include <ctype.h> /* isspace */
+#include <sys/wait.h> /* definierar bland annat WIFEXITED */
+#include <unistd.h> /* definierar bland annat pipe() */
 
 /* Define booleans */
 typedef int bool;
@@ -17,6 +19,7 @@ char command_prompt[100]; /* The command string that the user inputs */
 bool background = false; /* Whether the current command should be run in background or not */
 char* args[10]; /* The command arguments as an array */
 int argc = 0; /* The number of arguments to the command */
+pid_t child_pid;
 
 int main(int argc, char **argv, char **envp)
 {
@@ -32,11 +35,40 @@ int main(int argc, char **argv, char **envp)
 			
 		} else {
 			/* execute system command in child process */
+			child_pid = fork();
 
+			if (child_pid == -1) { /* Parent process, fork failed */
+				exit(1); /* TODO print error message and continue? */
+
+			} else if (child_pid == 0) { /* Child process */
+
+				/* Execute the command */
+				execvp(args[0], args);
+
+				/* This code is only reached if there is an error in execvp */
+				perror("Exec failed");
+				exit(1); /* Should we kill the shell here, or just print an error and go on with our lives? */
+
+			} 
+
+			/* TODO Wait for process if it is not background */
+			int status;
+			if (waitpid(child_pid, &status, 0) == -1) {
+				exit(1); /* TODO print error message and continue? */
+			}
+			if (WIFEXITED(status)) { 
+				int child_status = WEXITSTATUS(status); /* Check which signal child process exited with */
+				if (child_status != 0) { /* Something went wrong in child process */
+					fprintf(stderr, "Child process (%s) failed with exit code %d\n", args[0], child_status);
+				}
+			} else if (WIFSIGNALED(status)) {
+				int child_status = WTERMSIG(status); /* Child process ended by signal */
+				fprintf(stderr, "Child process (%s) terminated by signal %d\n", args[0], child_status);
+			}
 
 		}
-		//print info about finished command
-		//check if background processes have finished
+		/* TODO print info about finished command */
+		/* TODO check if background processes have finished */
 	}
 }
 
